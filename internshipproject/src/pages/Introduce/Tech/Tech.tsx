00756378abd43f439aa2cf4255bfa3cc82f4edbc
gwanhun <apple@APPLEui-MacBookPro.local>
2022-11-15 10:40:43 +0900

    ADD: 초기세팅

---
+++ internshipproject/src/pages/Introduce/Tech/Tech.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tech = () => {
+  return <div>기술소개</div>;
+};
+
+export default Tech;
---
+++ internshipproject/src/pages/News/News.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const News = () => {
+  return <div>뉴스페이지</div>;
+};
+
+export default News;
