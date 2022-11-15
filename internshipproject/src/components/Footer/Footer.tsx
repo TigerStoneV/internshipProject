9a3f758111d818ee1a8940ebd656cd3931f0df51
gwanhun <apple@APPLEui-MacBookPro.local>
2022-11-15 14:08:23 +0900

    ADD:Footer layout

--- internshipproject/src/components/Footer/Footer.tsx
+++ internshipproject/src/components/Footer/Footer.tsx
@@ -1,7 +1,77 @@
 import React from "react";
+import styled from "styled-components/macro";
+import variables from "../../styles/variables";
+import FooterOffCanvas from "./FooterOffcanvas";
 
 const Footer = () => {
-  return <div>푸터</div>;
+  return (
+    <>
+      <S.FooterBox>
+        <S.FooterCenter>
+          <S.FooterLogoBox>
+            <S.FooterLogo
+              src="http://www.star-pickers.com/html/img/logo_gray.png"
+              alt="별따러가자 로고"
+            />
+            <S.FooterLogo
+              src="http://www.star-pickers.com/html/img/logo_riderlog.png"
+              alt="라이더로그 로고"
+            />
+          </S.FooterLogoBox>
+          <S.FooterMessageBox>
+            <S.FooterMessage>서비스용 개인 정보처리 방침</S.FooterMessage>
+            <S.FooterMessage>위치기반 서비스 이용 약관</S.FooterMessage>
+          </S.FooterMessageBox>
+          <S.ButtonBox>
+            {["bottom"].map((placement, idx) => (
+              <FooterOffCanvas
+                key={idx}
+                placement={placement}
+                name={placement}
+              />
+            ))}
+          </S.ButtonBox>
+        </S.FooterCenter>
+      </S.FooterBox>
+    </>
+  );
 };
 
 export default Footer;
+
+const S = {
+  FooterBox: styled.div`
+    ${variables.flex()}
+    width: 100%;
+    height: 120px;
+    background-color: #212121;
+  `,
+
+  FooterCenter: styled.div`
+    ${variables.flex()}
+    width: 1400px;
+    height: 100%;
+  `,
+  FooterLogoBox: styled.div`
+    ${variables.flex()}
+    width: 30%;
+  `,
+
+  FooterLogo: styled.img`
+    width: 140px;
+    height: 55px;
+    margin: 0 40px 0 20px;
+  `,
+  FooterMessageBox: styled.div`
+    width: 700px;
+  `,
+
+  FooterMessage: styled.div`
+    color: rgb(250, 250, 250);
+    font-size: 12px;
+  `,
+  ButtonBox: styled.div`
+    ${variables.flex()}
+    width:200px;
+  `,
+};
